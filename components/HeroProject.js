e9adcf343561506ebf00a079db8e0d6b306c606d
mLeclercDev <marin.leclerc.dev@gmail.com>
2024-04-12 17:13:25 +0200

    Test split titre

--- components/HeroProject.js
+++ components/HeroProject.js
@@ -13,33 +13,43 @@ const HeroProject = ({ image, title }) => {
 
   useEffect(() => {
     if (!isRendered) return;
-    
+
     const tl = gsap.timeline();
 
     gsap.set(wrapperImageRef.current,{ className: "fit-cover" })
-    gsap.set("h1 .line",{ y: "100%" })
 
     tl.to(wrapperImageRef.current, { className: "fit-cover view", delay: 0.55 }) // cibler les enfants de wrapperImageRef.current
-    .to("h1 .line", { y: "0%", ease: 'power4.inOut', duration: 1, delay: 0.55, stagger: 0 });
+
+    const timer = setTimeout(() => {
+      var lines = textRef.current.querySelectorAll(".line");
+      console.log("lines : ", lines)
+
+      gsap.set(lines,{ y: "100%" })
+  
+      tl.to(lines, { y: "0%", ease: 'power4.inOut', duration: 1, delay: 0.55, stagger: 0 });
+    }, 555); // Délai en millisecondes (1000 ms = 1 seconde)
+  
+    // Retour de fonction de nettoyage pour annuler le timer si le composant est démonté avant l'exécution du code
+    return () => clearTimeout(timer);
   }, [isRendered, image, title]);
 
   useEffect(() => {
     setIsRendered(true);
+    console.log(" setIsRendered(true);")
   }, [textRef, wrapperImageRef]);
 
   return (
     <div className="hero-project">
       <div className='content-wrapper'>
         <h1>
-{/*           <SplitText
+          <SplitText
             LineWrapper={({ children }) =><div className="line-wrapper"><span className="line">{children}</span></div>}
             WordWrapper={({ children }) => <>{children}</>}
             LetterWrapper={({ children }) => <>{children}</>}
             ref={textRef}
           >
             {title}
-          </SplitText> */}
-          <AnimatedTitle textRef={textRef} >{title}</AnimatedTitle>
+          </SplitText> 
         </h1>
         <div className='wrapper-image'>
           <Image className='fit-cover' ref={wrapperImageRef} src={`https:${image}`}alt="Ma belle image" width={1149} height={520} />
