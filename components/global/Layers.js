576141c8633b814178707150687c791888326680
mLeclercDev <marin.leclerc.dev@gmail.com>
2024-04-15 11:36:03 +0200

    Changement des layers images

--- components/global/Layers.js
+++ components/global/Layers.js
@@ -2,26 +2,25 @@ import React from "react";
 import Image from 'next/image';
 
 const Layers = () => {
-
     return (
       <div className="layers">
         <div className="layers__item">
-            <Image className='fit-cover' src="/Images/cover-image-1.png" alt="Mon image" width={1149} height={520} />
+            <Image className='fit-cover' src="/Images/layer-1.png" alt="Mon image" width={1150} height={520} />
         </div>
         <div className="layers__item">
-            <Image className='fit-cover' src="/Images/cover-image-2.png" alt="Mon image" width={1149} height={520} />
+            <Image className='fit-cover' src="/Images/layer-2.png" alt="Mon image" width={1150} height={520} />
         </div>
         <div className="layers__item">
-            <Image className='fit-cover' src="/Images/cover-image-3.png" alt="Mon image" width={1149} height={520} />
+            <Image className='fit-cover' src="/Images/layer-3.png" alt="Mon image" width={1150} height={520} />
         </div>
         <div className="layers__item">
-            <Image className='fit-cover' src="/Images/cover-image-6.png" alt="Mon image" width={1149} height={520} />
+            <Image className='fit-cover' src="/Images/layer-6.png" alt="Mon image" width={1150} height={520} />
         </div>
         <div className="layers__item">
-            <Image className='fit-cover' src="/Images/cover-image-5.png" alt="Mon image" width={1149} height={520} />
+            <Image className='fit-cover' src="/Images/layer-5.png" alt="Mon image" width={1150} height={520} />
         </div>
         <div className="layers__item">
-            <Image className='fit-cover' src="/Images/cover-image-4.png" alt="Mon image" width={1149} height={520} />
+            <Image className='fit-cover' src="/Images/layer-4.png" alt="Mon image" width={1150} height={520} />
         </div>
       </div>
     );
