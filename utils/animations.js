7933cc0be9e671c86430f7944c1cf10737d83304
mLeclercDev <marin.leclerc.dev@gmail.com>
2025-09-04 10:21:54 +0200

    footer 0 resp

--- utils/animations.js
+++ utils/animations.js
@@ -12,11 +12,12 @@ export const animatePageOut = (href, router) => {
     const tl = gsap.timeline()
 
     gsap.set("main", { opacity: 0, delay: 1 })
+    gsap.set("footer", { opacity: 0, delay: 1 })
     //gsap.set(".hero-project img",{ className: "fit-cover", delay: 0.5 })
 
     tl.to([banners], {
       duration: 1, stagger: 0.1, className: "layers__items in", onComplete: () => {
-        gsap.delayedCall(0.75, () => { router.push(href) })
+        gsap.delayedCall(0.25, () => { router.push(href) })
       },})
   }
 } 
