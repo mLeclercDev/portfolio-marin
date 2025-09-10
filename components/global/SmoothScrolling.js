0ea01b37c91ce8ed8471fe148180dd54025a2fbd
mLeclercDev <marin.leclerc.dev@gmail.com>
2025-09-10 12:15:10 +0200

    lenis update

--- components/global/SmoothScrolling.js
+++ components/global/SmoothScrolling.js
@@ -7,7 +7,7 @@ function SmoothScrolling({ children, infinite }) {
   const lenisRef = useRef(null);
 
   return (
-    <ReactLenis ref={lenisRef} root options={{ lerp: 0.075, duration: 2, smoothTouch: true, infinite: infinite }}>
+    <ReactLenis ref={lenisRef} root options={{ lerp: 0.1, duration: 1.2, smoothTouch: true, infinite: infinite }}>
       {children}
     </ReactLenis>
   );
