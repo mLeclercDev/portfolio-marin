65f378d19f436f97e0f4f86387bab3999b3df17d
mLeclercDev <marin.leclerc.dev@gmail.com>
2025-09-15 09:38:15 +0200

    priority 2

--- pages/404.js
+++ pages/404.js
@@ -19,7 +19,7 @@ export default function Custom404() {
                 <h1>404</h1>
                 <div onClick={() => handleClick()} className='arrow-link cs-scale'>
                     <span className='arrow-span'>
-                    Page d'accueil
+                    Page d&apos;accueil
                     </span>
                     <div className="arrow">
                         <svg className="first" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 3.33333H14.31L0.143334 17.5L2.5 19.8567L16.6667 5.69V20H20V0H0V3.33333Z" fill="#FCFBF6"></path></svg>
