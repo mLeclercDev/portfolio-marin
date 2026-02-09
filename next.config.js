5029dc53b312f3c3809a9b7281075c5129814e5a
mLeclercDev <marin.leclerc.dev@gmail.com>
2026-02-09 17:56:57 +0100

    fixe bug

--- next.config.js
+++ next.config.js
@@ -1,32 +1,15 @@
 // next.config.js
-const path = require('path');
-
-module.exports = {
-  webpack: (config, { isServer }) => {
-    // Ajouter le chargeur Sass pour la gestion des fichiers .scss
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader',
-      ],
-      include: path.resolve(__dirname, './styles'),
-    });
-
-
-    // Ajouter le chargeur CSS pour la gestion des fichiers .css
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-      ],
-    });
-
-    return config;
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ['images.ctfassets.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        pathname: '/**',
+      },
+    ],
   },
 };
+
+module.exports = nextConfig;
